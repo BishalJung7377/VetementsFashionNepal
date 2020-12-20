ebfd70972e901717d37f13a63a61703dd92869a4
BishalJung <52627217+BsalT800@users.noreply.github.com>
2020-12-20 12:36:39 +0545

    Initalized Project

---
+++ app/src/main/java/com/bishaljung/vetementsfashionnepal/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bishaljung.vetementsfashionnepal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
