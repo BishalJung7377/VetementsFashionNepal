9d954c3e410f65227a53595c0cc76d5000930963
BishalJung <52627217+BsalT800@users.noreply.github.com>
2021-01-17 12:01:32 +0545

    Splash Screen is added to our project with customized background and change in mainfest file so that to display splash screen

--- app/src/main/java/com/bishaljung/vetementsfashionnepal/Fragments/ProfileFragments.kt
+++ app/src/main/java/com/bishaljung/vetementsfashionnepal/Fragments/ProfileFragments.kt
@@ -16,46 +16,75 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class ProfileFragments : Fragment() {
-//    private lateinit var imgbackIcn: ImageView
-//    private lateinit var tvRegister: TextView
-//    private lateinit var userprofileimage: ImageView
-//    private lateinit var tvuserprofilename: TextView
-//    private lateinit var tvuserprofileemail: TextView
-//    private lateinit var totalitemcrdview: CardView
-//    private lateinit var itemCardView: ImageView
-//    private lateinit var tvuseritems: TextView
-//    private lateinit var tvitemnumbers: TextView
-//    private lateinit var tveditFname: TextInputLayout
-//    private lateinit var eteditFname: TextInputEditText
-//    private lateinit var tveditEmail: TextInputLayout
-//    private lateinit var eteditEmail: TextInputEditText
-//    private lateinit var tveditpass: TextInputLayout
-//    private lateinit var eteditpass: TextInputEditText
-//    private lateinit var tveditnumb: TextInputLayout
-//    private lateinit var eteditnumb: TextInputEditText
-//    private lateinit var btnupdate: Button
+    private lateinit var imgbackIcn: ImageView
+    private lateinit var tvRegister: TextView
+    private lateinit var userprofileimage: ImageView
+    private lateinit var tvuserprofilename: TextView
+    private lateinit var tvuserprofileemail: TextView
+    private lateinit var totalitemcrdview: CardView
+    private lateinit var itemCardView: ImageView
+    private lateinit var tvuseritems: TextView
+    private lateinit var tvitemnumbers: TextView
+    private lateinit var tveditFname: TextInputLayout
+    private lateinit var eteditFname: TextInputEditText
+    private lateinit var tveditEmail: TextInputLayout
+    private lateinit var eteditEmail: TextInputEditText
+    private lateinit var tveditpass: TextInputLayout
+    private lateinit var eteditpass: TextInputEditText
+    private lateinit var tveditnumb: TextInputLayout
+    private lateinit var eteditnumb: TextInputEditText
+    private lateinit var btnupdate: Button
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(
-            com.bishaljung.vetementsfashionnepal.R.layout.activity_fragment_profile,
-            container,
-            false
-        )
+        val view = inflater.inflate(R.layout.activity_fragment_profile, container, false)
+        imgbackIcn = view.findViewById(R.id.imgbackIcn)
+        tvRegister = view.findViewById(R.id.tvRegister)
+        userprofileimage = view.findViewById(R.id.userprofileimage)
+        tvuserprofilename = view.findViewById(R.id.tvuserprofilename)
+        tvuserprofileemail= view.findViewById(R.id.tvuserprofileemail)
+        totalitemcrdview = view.findViewById(R.id.totalitemcrdview)
+        itemCardView = view.findViewById(R.id.itemCardView)
+        tvuseritems = view.findViewById(R.id.tvuseritems)
+        tvitemnumbers = view.findViewById(R.id.tvitemnumbers)
+        tveditFname = view.findViewById(R.id.tveditFname)
+        eteditFname = view.findViewById(R.id.eteditFname)
+        tveditEmail = view.findViewById(R.id.tveditEmail)
+        eteditEmail = view.findViewById(R.id.eteditEmail)
+        tveditpass = view.findViewById(R.id.tveditpass)
+        eteditpass = view.findViewById(R.id.eteditpass)
+        tveditnumb = view.findViewById(R.id.tveditnumb)
+        eteditnumb = view.findViewById(R.id.eteditnumb)
+        btnupdate = view.findViewById(R.id.btnupdate)
+
+        imgbackIcn.setOnClickListener {
+            val intent = Intent(context, HomeFragments::class.java )
+            startActivity(intent)
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return view
+
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-//        imgbackIcn = requireView().findViewById(R.id.imgbackIcn)
-//
-//
-//        imgbackIcn.setOnClickListener {
-//            //  val intent = Intent(this, HomeFragments::class.java)
-//            //startActivity(intent)
-//        }
 
-    }
 }
